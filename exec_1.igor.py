970f346ed63f3aa5c12b35538204348441fb9b8c
E13-star <evelen.libras@gmail.com>
2024-10-28 22:39:30 -0300

    Add files via upload

---
+++ exec_1.igor.py
@@ -0,0 +1,7 @@
+#Numero positivo ou negativo
+numero_positivo = float(input("Informe o numero:"))
+numero_negativo = float(input("Informe o numero:"))
+if numero_positivo > 0:
+    print("O numero é positivo")
+else:
+    print("O numero é negativo")
